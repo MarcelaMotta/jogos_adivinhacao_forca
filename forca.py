1384ae0f4914766de0c47cef45b195b2660df936
Marcela Motta <marcelamottadecastro@gmail.com>
2022-12-26 17:56:37 -0300

    Adding list of words, randomic choose and attemp limit number.

--- forca.py
+++ forca.py
@@ -1,26 +1,52 @@
+import random
+
 def jogar():
 
     print("Jogo da forca")
 
-    palavra_secreta = "banana"
+    palavras = []
+
+    with open("palavras.txt") as arquivo:
+        for linha in arquivo:
+            linha = linha.strip()
+            palavras.append(linha)
+
+    numero_randomico = random.randrange(0,len(palavras))
+    palavra_secreta = palavras[numero_randomico]
+
+    palavra_secreta = palavra_secreta.upper()
+    letras_acertadas = ["_" for letra in palavra_secreta]
 
     enforcou = False
     acertou = False
+    erros = 0
 
     while (not enforcou and not acertou):
 
         chute = input("Qual a letra?")
-        chute = chute.strip()
+        chute = chute.strip().upper()
 
-        index = 0
-        for letra in palavra_secreta:
-            if chute.upper() == letra.upper():
-                print("Encontrei a letra '{}' na posição {}".format(chute, index))
-            index += 1
+        if chute in palavra_secreta:
+            index = 0
+            for letra in palavra_secreta:
+                if chute == letra:
+                    print("Encontrei a letra '{}' na posição {}".format(chute, index))
+                    letras_acertadas[index] = letra
+                index += 1
+        else:
+            erros += 1
+
+        enforcou = erros == 6
+        acertou = "_" not in letras_acertadas
+        print(letras_acertadas)
+
+    if acertou:
+        print("Parabéns! Você acertou!")
+    else:
+        print("Você excedeu o número de tentativas.")
 
 
-    print("Qual o nível de dificuldade do jogo?")
-    dificuldade = int(input("Digite (1) Fácil (2) Médio (3) Difícil: "))
+    print("Fim do jogo!")
 
 
 
